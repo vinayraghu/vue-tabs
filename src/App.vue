2d3adde15f56a3b4b87efcb40d5802ac6f35ac44
Vinay Raghu <hello@vinayraghu.com>
2020-08-28 08:32:13 -0400

    tabs in vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Tabs />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Tabs from './components/Tabs.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Tabs
   }
 }
 </script>
---
+++ src/components/Tabs.vue
@@ -0,0 +1,50 @@
+<template>
+  <article>
+    <section class="tab-titles">
+      <div class="chewy-tab-title" @click="setActive('apple')">Apple</div>
+      <div class="chewy-tab-title" @click="setActive('banana')">Banana</div>
+      <div class="chewy-tab-title" @click="setActive('cherry')">Cherry</div>
+    </section>
+    <section>
+      <div
+        class="chewy-tab-panel"
+        :class="activeTab === 'apple' && 'chewy-tab-panel--active'"
+      >
+        Apples are good
+      </div>
+      <div
+        class="chewy-tab-panel"
+        :class="activeTab === 'banana' && 'chewy-tab-panel--active'"
+      >
+        Bananas are great
+      </div>
+      <div
+        class="chewy-tab-panel"
+        :class="activeTab === 'cherry' && 'chewy-tab-panel--active'"
+      >
+        Cherries are fantastic
+      </div>
+    </section>
+  </article>
+</template>
+<script>
+export default {
+  name: "Tabs",
+  data() {
+    return {
+      activeTab: "apple",
+    };
+  },
+  methods: {
+    setActive(str) {
+      this.activeTab = str;
+    },
+  },
+};
+</script>
+<style lang="scss">
+@import "./chewy-tabs.css";
+.tab-titles {
+  // display: flex;
+}
+</style>
